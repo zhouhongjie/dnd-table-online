cb50afce60e3b62d6528197093894a82de158d52
christophe.wouters.gspace@gmail.com <christophe.wouters.gspace@gmail.com@79b56415-8d3c-a410-a269-1c82d5f13b0d>
2014-01-29 09:47:48 +0000

    unit tests: todo

--- DndTable.Core.Test/UnitTests/AttackRollTest.cs
+++ DndTable.Core.Test/UnitTests/AttackRollTest.cs
@@ -12,31 +12,31 @@ namespace DndTable.Core.Test.UnitTests
         [Test]
         public void SuccessTest()
         {
-            
+            throw new NotImplementedException();
         }
 
         [Test]
         public void AutoSuccessTest()
         {
-            
+            throw new NotImplementedException();
         }
 
         [Test]
         public void FailTest()
         {
-            
+            throw new NotImplementedException();
         }
 
         [Test]
         public void AutoFailTest()
         {
-            
+            throw new NotImplementedException();
         }
 
         [Test]
         public void ThreatTest()
         {
-            
+            throw new NotImplementedException();
         }
     }
 }
