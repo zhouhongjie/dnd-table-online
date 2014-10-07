3eb87ef69c5ab8d6759fe586c4623ecaff5c1e87
christophe.wouters.gspace@gmail.com <christophe.wouters.gspace@gmail.com@79b56415-8d3c-a410-a269-1c82d5f13b0d>
2014-10-07 20:41:02 +0000

    Donjon generator map import

---
+++ trunk/DndTable.Core.Test/UserTests/DonjonMapReaderTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DndTable.Core.Entities;
+using DndTable.Core.Persistence;
+using NUnit.Framework;
+
+namespace DndTable.Core.Test.UserTests
+{
+    [TestFixture]
+    public class DonjonMapReaderTest
+    {
+        [Test]
+        public void Test()
+        {
+            var reader = new DonjonMapReader();
+            reader.SetDonjonFolder(@"E:\Data\Projects\DndTableOnline\Data\Donjon\");
+
+            int maxX, maxY;
+            List<BaseEntity> entities;
+
+            var name = "The Sepulcher of Adamant Terror 01 (tsv)";
+
+            Assert.That(reader.LoadBoard(name, out maxX, out maxY, out entities));
+
+            // Save to xml
+            var repository = new Repository(@"E:\Data\Projects\DndTableOnline\Data\Maps\", @"E:\Data\Projects\DndTableOnline\Data\Characters\");
+            repository.SaveBoard("test2", maxX, maxY, entities);
+        }
+    }
+}
---
+++ trunk/DndTable.Core/Persistence/DonjonMapReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DndTable.Core.Entities;
+using DndTable.Core.Factories;
+
+namespace DndTable.Core.Persistence
+{
+    /// <summary>
+    /// load TSV map exports from: http://donjon.bin.sh/
+    /// </summary>
+    public class DonjonMapReader
+    {
+        private string _dungeonFolder = @"../Data/Donjon/";
+
+        public void SetDonjonFolder(string folder)
+        {
+            _dungeonFolder = folder;
+        }
+
+        internal bool LoadBoard(string name, out int maxX, out int maxY, out List<BaseEntity> entities)
+        {
+            // Init out params
+            maxX = 0;
+            maxY = 0;
+            entities = null;
+
+            // Check file
+            var filename = _dungeonFolder + name + ".txt";
+            if (!File.Exists(filename))
+                return false;
+
+            entities = new List<BaseEntity>();
+
+            var allLines = File.ReadAllLines(filename);
+            maxY = allLines.Length;
+
+            for(int y=0; y < allLines.Length; y++)
+            {
+                var line = allLines[y];
+                var cells = line.Split('\t');
+                maxX = cells.Length;
+
+                for (int x = 0; x < cells.Length; x++)
+                {
+                    var cell = cells[x];
+                    BaseEntity newEntity = null;
+
+                    // Walls
+                    if (string.IsNullOrEmpty(cell))
+                    {
+                        newEntity = Factory.CreateEntity(EntityTypeEnum.Wall) as BaseEntity;
+                    }
+
+                    // Doors
+                    if (cell.Contains("D"))
+                    {
+                        newEntity = Factory.CreateEntity(EntityTypeEnum.Door) as BaseEntity;
+                    }
+
+                    if (newEntity != null)
+                    {
+                        newEntity.Position = Position.Create(x, y);
+                        entities.Add(newEntity);
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
